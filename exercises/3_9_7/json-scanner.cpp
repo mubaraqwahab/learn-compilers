37a60022d67ce9fe086138861393013363b24b6d
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-12-22 18:43:12 +0100

    add indent-namespaces flag to astyle

--- exercises/3_9_7/json-scanner.cpp
+++ exercises/3_9_7/json-scanner.cpp
@@ -7,151 +7,150 @@ using namespace std;
 
 namespace json
 {
-
-// for debugging
-string tok_as_string(Token tok)
-{
-  switch (tok) {
-  case Token::lbracket:
-    return "lbracket";
-  case Token::lcurly:
-    return "lcurly";
-  case Token::rbracket:
-    return "rbracket";
-  case Token::rcurly:
-    return "rcurly";
-  case Token::colon:
-    return "colon";
-  case Token::comma:
-    return "comma";
-  case Token::string:
-    return "string";
-  case Token::number:
-    return "number";
-  case Token::boolean:
-    return "boolean";
-  case Token::null:
-    return "null";
-  default:
-    throw std::invalid_argument("Unknown token type");
-  }
-}
-
-Scanner::Scanner(istream& is) : input_stream(is.rdbuf())
-{
-  // Initial state
-  state = State::value;
-}
-
-Token Scanner::next_token()
-{
-  while (available_tokens.size() == 0) {
-    get_state_handler();
+  // for debugging
+  string tok_as_string(Token tok)
+  {
+    switch (tok) {
+    case Token::lbracket:
+      return "lbracket";
+    case Token::lcurly:
+      return "lcurly";
+    case Token::rbracket:
+      return "rbracket";
+    case Token::rcurly:
+      return "rcurly";
+    case Token::colon:
+      return "colon";
+    case Token::comma:
+      return "comma";
+    case Token::string:
+      return "string";
+    case Token::number:
+      return "number";
+    case Token::boolean:
+      return "boolean";
+    case Token::null:
+      return "null";
+    default:
+      throw std::invalid_argument("Unknown token type");
+    }
   }
 
-  auto token = available_tokens.front();
-  available_tokens.pop_front();
-  return token;
-}
-
-char Scanner::next_char()
-{
-  char c;
-  input_stream >> c;
-  return c;
-}
-
-void Scanner::get_state_handler()
-{
-  switch (state) {
-  case State::value:
-    value_handler();
-    break;
-  case State::string:
-    string_handler();
-    break;
-  case State::string_escape:
-    string_escape_handler();
-    break;
-  case State::literal_name:
-    literal_name_handler();
-    break;
-  case State::number_start:
-    number_start_handler();
-    break;
-  case State::number_int:
-    number_int_handler();
-    break;
-  case State::number_after_int:
-    number_after_int_handler();
-    break;
-  case State::number_fraction_start:
-    number_fraction_start_handler();
-    break;
-  case State::number_fraction:
-    number_fraction_handler();
-    break;
-  case State::number_exponent_sign:
-    number_exponent_sign_handler();
-    break;
-  case State::number_exponent_start:
-    number_exponent_start_handler();
-    break;
-  case State::number_exponent:
-    number_exponent_handler();
-    break;
-  default:
-    throw;
+  Scanner::Scanner(istream& is) : input_stream(is.rdbuf())
+  {
+    // Initial state
+    state = State::value;
   }
-}
 
-void Scanner::value_handler()
-{
-  char c = next_char();
-  if (c == '{') {
-    available_tokens.push_back(Token::lcurly);
-  } else if (c == '[') {
-    available_tokens.push_back(Token::lbracket);
-  } else if (c == '}') {
-    available_tokens.push_back(Token::rcurly);
-  } else if (c == ']') {
-    available_tokens.push_back(Token::rbracket);
-  } else if (c == ':') {
-    available_tokens.push_back(Token::colon);
-  } else if (c == ',') {
-    available_tokens.push_back(Token::comma);
-  } else if (c == '"') {
-    //
-  } else if (c == '-') {
-  } else if (c >= '0' && c <= '9') {
-  } else if (c >= 'a' && c <= 'z') {
-  } else if (isspace(c)) {
+  Token Scanner::next_token()
+  {
+    while (available_tokens.size() == 0) {
+      handle_current_state();
+    }
+
+    auto token = available_tokens.front();
+    available_tokens.pop_front();
+    return token;
   }
-  /* else if end of file */
-  else {
+
+  char Scanner::next_char()
+  {
+    char c;
+    input_stream >> c;
+    return c;
   }
-}
 
-void Scanner::string_handler() {}
+  void Scanner::handle_current_state()
+  {
+    switch (state) {
+    case State::value:
+      value_handler();
+      break;
+    case State::string:
+      string_handler();
+      break;
+    case State::string_escape:
+      string_escape_handler();
+      break;
+    case State::literal_name:
+      literal_name_handler();
+      break;
+    case State::number_start:
+      number_start_handler();
+      break;
+    case State::number_int:
+      number_int_handler();
+      break;
+    case State::number_after_int:
+      number_after_int_handler();
+      break;
+    case State::number_fraction_start:
+      number_fraction_start_handler();
+      break;
+    case State::number_fraction:
+      number_fraction_handler();
+      break;
+    case State::number_exponent_sign:
+      number_exponent_sign_handler();
+      break;
+    case State::number_exponent_start:
+      number_exponent_start_handler();
+      break;
+    case State::number_exponent:
+      number_exponent_handler();
+      break;
+    default:
+      throw;
+    }
+  }
 
-void Scanner::string_escape_handler() {}
+  void Scanner::value_handler()
+  {
+    char c = next_char();
+    if (c == '{') {
+      available_tokens.push_back(Token::lcurly);
+    } else if (c == '[') {
+      available_tokens.push_back(Token::lbracket);
+    } else if (c == '}') {
+      available_tokens.push_back(Token::rcurly);
+    } else if (c == ']') {
+      available_tokens.push_back(Token::rbracket);
+    } else if (c == ':') {
+      available_tokens.push_back(Token::colon);
+    } else if (c == ',') {
+      available_tokens.push_back(Token::comma);
+    } else if (c == '"') {
+      //
+    } else if (c == '-') {
+    } else if (c >= '0' && c <= '9') {
+    } else if (c >= 'a' && c <= 'z') {
+    } else if (isspace(c)) {
+    }
+    /* else if end of file */
+    else {
+    }
+  }
 
-void Scanner::literal_name_handler() {}
+  void Scanner::string_handler() {}
 
-void Scanner::number_start_handler() {}
+  void Scanner::string_escape_handler() {}
 
-void Scanner::number_int_handler() {}
+  void Scanner::literal_name_handler() {}
 
-void Scanner::number_after_int_handler() {}
+  void Scanner::number_start_handler() {}
 
-void Scanner::number_fraction_start_handler() {}
+  void Scanner::number_int_handler() {}
 
-void Scanner::number_fraction_handler() {}
+  void Scanner::number_after_int_handler() {}
 
-void Scanner::number_exponent_sign_handler() {}
+  void Scanner::number_fraction_start_handler() {}
 
-void Scanner::number_exponent_start_handler() {}
+  void Scanner::number_fraction_handler() {}
 
-void Scanner::number_exponent_handler() {}
+  void Scanner::number_exponent_sign_handler() {}
+
+  void Scanner::number_exponent_start_handler() {}
+
+  void Scanner::number_exponent_handler() {}
 
 }
--- exercises/3_9_7/json-scanner.h
+++ exercises/3_9_7/json-scanner.h
@@ -7,63 +7,63 @@
 
 namespace json
 {
-enum class Token {
-  lbracket,
-  lcurly,
-  rbracket,
-  rcurly,
-  colon,
-  comma,
-  string,
-  number,
-  boolean,
-  null
-};
+  enum class Token {
+    lbracket,
+    lcurly,
+    rbracket,
+    rcurly,
+    colon,
+    comma,
+    string,
+    number,
+    boolean,
+    null
+  };
 
-// for debugging
-std::string tok_as_string(Token tok);
+  // for debugging
+  std::string tok_as_string(Token tok);
 
-enum class State {
-  value,
-  string,
-  string_escape,
-  literal_name,
-  number_start,
-  number_int,
-  number_after_int,
-  number_fraction_start,
-  number_fraction,
-  number_exponent_sign,
-  number_exponent_start,
-  number_exponent
-};
+  enum class State {
+    value,
+    string,
+    string_escape,
+    literal_name,
+    number_start,
+    number_int,
+    number_after_int,
+    number_fraction_start,
+    number_fraction,
+    number_exponent_sign,
+    number_exponent_start,
+    number_exponent
+  };
 
-class Scanner
-{
-public:
-  Scanner(std::istream& is);
-  Token next_token();
+  class Scanner
+  {
+  public:
+    Scanner(std::istream& is);
+    Token next_token();
 
-private:
-  std::istream input_stream;
-  State state;
-  std::deque<Token> available_tokens;
-  void get_state_handler();
-  char next_char();
-  // Handlers
-  void value_handler();
-  void string_handler();
-  void string_escape_handler();
-  void literal_name_handler();
-  void number_start_handler();
-  void number_int_handler();
-  void number_after_int_handler();
-  void number_fraction_start_handler();
-  void number_fraction_handler();
-  void number_exponent_sign_handler();
-  void number_exponent_start_handler();
-  void number_exponent_handler();
-};
+  private:
+    std::istream input_stream;
+    State state;
+    std::deque<Token> available_tokens;
+    void handle_current_state();
+    char next_char();
+    // Handlers
+    void value_handler();
+    void string_handler();
+    void string_escape_handler();
+    void literal_name_handler();
+    void number_start_handler();
+    void number_int_handler();
+    void number_after_int_handler();
+    void number_fraction_start_handler();
+    void number_fraction_handler();
+    void number_exponent_sign_handler();
+    void number_exponent_start_handler();
+    void number_exponent_handler();
+  };
 }
 
 #endif /* JSON_SCANNER_H */
