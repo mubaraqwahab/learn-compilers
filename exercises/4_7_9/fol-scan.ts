b6a0874e5126abab3eefe470057ab0f468165c96
Mubaraq Wahab <48601635+mubaraqwahab@users.noreply.github.com>
2022-07-06 17:06:13 +0100

    Refactor fol-scan to include token images
    Also refactor the type defs of tokens

--- exercises/4_7_9/fol-scan.ts
+++ exercises/4_7_9/fol-scan.ts
@@ -1,24 +1,24 @@
 export const createToken: Record<Token["type"], (...args: any[]) => Token> = {
   boolean(value: boolean, index: number) {
-    return { type: "boolean", value, index };
+    return { type: "boolean", value, index, image: value ? "T" : "F" };
   },
   andOp(index: number) {
-    return { type: "andOp", index };
+    return { type: "andOp", index, image: "&" };
   },
   orOp(index: number) {
-    return { type: "orOp", index };
+    return { type: "orOp", index, image: "|" };
   },
   impliesOp(index: number) {
-    return { type: "impliesOp", index };
+    return { type: "impliesOp", index, image: "->" };
   },
   notOp(index: number) {
-    return { type: "notOp", index };
+    return { type: "notOp", index, image: "!" };
   },
   groupStart(index: number) {
-    return { type: "groupStart", index };
+    return { type: "groupStart", index, image: "(" };
   },
   groupEnd(index: number) {
-    return { type: "groupEnd", index };
+    return { type: "groupEnd", index, image: ")" };
   },
   eof(index: number) {
     return { type: "eof", index };
@@ -65,12 +65,29 @@ export function scan(src: string): Token[] {
   return tokens;
 }
 
-export type Token =
-  | { type: "boolean"; value: boolean; index: number }
-  | { type: "andOp"; index: number }
-  | { type: "orOp"; index: number }
-  | { type: "impliesOp"; index: number }
-  | { type: "notOp"; index: number }
-  | { type: "groupStart"; index: number }
-  | { type: "groupEnd"; index: number }
-  | { type: "eof"; index: number };
+export type Token = BooleanToken | OperatorToken | EOFToken;
+
+interface BooleanToken extends IToken {
+  type: "boolean";
+  value: boolean;
+};
+
+interface OperatorToken extends IToken {
+  type:
+    | "andOp"
+    | "orOp"
+    | "impliesOp"
+    | "notOp"
+    | "groupStart"
+    | "groupEnd";
+}
+
+interface EOFToken extends IToken = {
+  type: "eof";
+};
+
+interface IToken {
+  type: string;
+  index: number;
+  image: string;
+}
