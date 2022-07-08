b506db7de40c518f5103f1326ee146d64bc8c520
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-07-08 13:52:39 +0100

    make regex-to-nfa work!

--- exercises/4_10/regex-to-nfa.test.ts
+++ exercises/4_10/regex-to-nfa.test.ts
@@ -1,3 +1,72 @@
-import { test } from "vitest";
+import { expect, test } from "vitest";
+import { type NFA, regexToNFA } from "./regex-to-nfa.js";
 
-test.skip("", () => {});
+test("can convert a simple regex to an NFA", () => {
+  const nfa: NFA = {
+    final: "s1",
+    transitions: new Set([["s0", "a", "s1"]]),
+  };
+  expect(regexToNFA(`a`)).toMatchObject(nfa);
+});
+
+test("can convert a concat to an NFA", () => {
+  const nfa: NFA = {
+    final: "s2",
+    transitions: new Set([
+      ["s0", "a", "s1"],
+      ["s1", "b", "s2"],
+    ]),
+  };
+  expect(regexToNFA(`ab`)).toMatchObject(nfa);
+});
+
+test("can convert a union to an NFA", () => {
+  const nfa: NFA = {
+    final: "s5",
+    transitions: new Set([
+      ["s0", "", "s1"],
+      ["s1", "a", "s2"],
+      ["s0", "", "s3"],
+      ["s3", "b", "s4"],
+      ["s2", "", "s5"],
+      ["s4", "", "s5"],
+    ]),
+  };
+  expect(regexToNFA(`a|b`)).toMatchObject(nfa);
+});
+
+test("can convert a kleene closure to an NFA", () => {
+  const nfa: NFA = {
+    final: "s3",
+    transitions: new Set([
+      ["s0", "", "s1"],
+      ["s1", "a", "s2"],
+      ["s2", "", "s1"],
+      ["s2", "", "s3"],
+      ["s0", "", "s3"],
+    ]),
+  };
+  expect(regexToNFA(`a*`)).toMatchObject(nfa);
+});
+
+test.skip("can convert a complex regex to an NFA", () => {
+  const nfa: NFA = {
+    final: "s10",
+    transitions: new Set([
+      ["s0", "", "s1"],
+      ["s1", "", "s2"],
+      ["s2", "a", "s3"],
+      ["s1", "", "s4"],
+      ["s4", "", "s5"],
+      ["s3", "", "s6"],
+      ["s5", "", "s6"],
+      ["s6", "", "s1"],
+      ["s6", "", "s7"],
+      ["s0", "", "s7"],
+      ["s7", "a", "s8"],
+      ["s8", "b", "s9"],
+      ["s9", "b", "s10"],
+    ]),
+  };
+  expect(regexToNFA(`(a|b)*abb`)).toMatchObject(nfa);
+});
--- exercises/4_10/regex-to-nfa.ts
+++ exercises/4_10/regex-to-nfa.ts
@@ -6,73 +6,126 @@ import { Node, parse } from "./regex-parser.js";
  */
 export function regexToNFA(regex: string) {
   const ast = parse(regex);
-  throw new Error("not implemented!");
-
-  // Visit each node
-  // TODO: learn visitor pattern!
-  // TODO: generate unique state names
+  return nodeToNFA(ast);
 }
 
-interface NFA {
-  initial: string;
-  final: string;
-  states:
-    | {
-        [state: string]: {
-          [char: string]: string;
-        };
-      }
-    | [string, string, string];
+/**
+ * Convert a regular expression AST node to an NFA
+ * using the McNaughton-Yamada-Thompson algorithm.
+ */
+function nodeToNFA(node: Node): NFA {
+  // TODO: Refactor and cleanup!
+
+  switch (node.type) {
+    case "Literal": {
+      const nfa: NFA = {
+        final: "s1",
+        transitions: new Set([["s0", node.value, "s1"]]),
+      };
+      return nfa;
+    }
+
+    case "Concat": {
+      const leftNFA = nodeToNFA(node.left);
+      const rightNFA = nodeToNFA(node.right);
+      const transitions = new Set(leftNFA.transitions);
+
+      const leftFinalId = id(leftNFA.final);
+      const rightFinalId = id(rightNFA.final);
+
+      rightNFA.transitions.forEach(([from, char, to]) => {
+        const newFrom: NFAState = `s${id(from) + leftFinalId}`;
+        const newTo: NFAState = `s${id(to) + leftFinalId}`;
+        transitions.add([newFrom, char, newTo]);
+      });
+
+      const nfa: NFA = {
+        final: `s${rightFinalId + leftFinalId}`,
+        transitions,
+      };
+
+      return nfa;
+    }
+
+    case "Union": {
+      const leftNFA = nodeToNFA(node.left);
+      const rightNFA = nodeToNFA(node.right);
+
+      const transitions: NFA["transitions"] = new Set([["s0", "", "s1"]]);
+
+      leftNFA.transitions.forEach(([from, char, to]) => {
+        const newFrom: NFAState = `s${id(from) + 1}`;
+        const newTo: NFAState = `s${id(to) + 1}`;
+        transitions.add([newFrom, char, newTo]);
+      });
+
+      const rightOffset = id(leftNFA.final) + 1 + 1;
+      transitions.add(["s0", "", `s${rightOffset}`]);
+
+      rightNFA.transitions.forEach(([from, char, to]) => {
+        const newFrom: NFAState = `s${id(from) + rightOffset}`;
+        const newTo: NFAState = `s${id(to) + rightOffset}`;
+        transitions.add([newFrom, char, newTo]);
+      });
+
+      const final: NFAState = `s${id(rightNFA.final) + rightOffset + 1}`;
+
+      const newLeftFinal: NFAState = `s${id(leftNFA.final) + 1}`;
+      const newRightFinal: NFAState = `s${id(rightNFA.final) + rightOffset}`;
+      transitions.add([newLeftFinal, "", final]);
+      transitions.add([newRightFinal, "", final]);
+
+      const nfa: NFA = { final, transitions };
+
+      return nfa;
+    }
+
+    case "Kleene": {
+      const argNFA = nodeToNFA(node.argument);
+      const transitions: NFA["transitions"] = new Set([["s0", "", "s1"]]);
+
+      argNFA.transitions.forEach(([from, char, to]) => {
+        const newFrom: NFAState = `s${id(from) + 1}`;
+        const newTo: NFAState = `s${id(to) + 1}`;
+        transitions.add([newFrom, char, newTo]);
+      });
+
+      const newArgFinal: NFAState = `s${id(argNFA.final) + 1}`;
+      const final: NFAState = `s${id(argNFA.final) + 2}`;
+      transitions.add([newArgFinal, "", "s1"]);
+      transitions.add([newArgFinal, "", final]);
+      transitions.add(["s0", "", final]);
+
+      const nfa: NFA = { final, transitions };
+      return nfa;
+    }
+
+    case "RegEx": {
+      return nodeToNFA(node.body);
+    }
+  }
 }
 
-function play() {
-  let nfa: NFA;
-
-  // r = a
-  nfa = {
-    initial: "s1",
-    final: "s2",
-    states: {
-      s1: { a: "s2" },
-      s2: {},
-    },
-  };
-
-  // r = b
-  nfa = {
-    initial: "s3",
-    final: "s4",
-    states: {
-      s3: { b: "s4" },
-      s4: {},
-    },
-  };
-
-  // r = ab
-  nfa = {
-    initial: "s1",
-    final: "s4",
-    states: {
-      s1: { a: "s2" },
-      s2: { b: "s4" },
-      s4: {},
-    },
-  };
-
-  // r = a|b
-  nfa = {
-    initial: "s0",
-    final: "",
-    states: {
-      // TODO: What's the best way to write this that allows duped keys?
-      // Consider: [['s0', '', 's1'], ['s0', '', 's3'], ...]
-      // Should you omit the tuple for the final state if it's empty?
-      s0: { "": "s1", "": "s3" },
-      s1: { a: "s2" },
-      s2: { "": "s5" },
-      s3: { b: "s4" },
-      s4: { "": "s5" },
-      s5: {},
-    },
-  };
+/**
+ * Get the id of a given NFA state.
+ * @example
+ * id("s1") === 1
+ */
+function id(state: NFAState): number {
+  return +state.slice(1);
 }
+
+export interface NFA {
+  // No need for an explicit initial state;
+  // the initial state will always be 's0'
+  final: NFAState;
+  /**
+   * An set of tuples [S1, char, S2] where each tuple
+   * denotes a transition from state S1 to state S2 through char.
+   * This property is a *set* not an *array* because the order of
+   * the tuples don't matter.
+   */
+  transitions: Set<[NFAState, string, NFAState]>;
+}
+
+type NFAState = `s${number}`;
