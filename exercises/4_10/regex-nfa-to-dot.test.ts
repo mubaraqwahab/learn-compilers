fca635d75085185267f2709af42f86bc899fbad5
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-07-09 16:42:54 +0100

    serialise dot text in snapshots better

--- exercises/4_10/regex-nfa-to-dot.test.ts
+++ exercises/4_10/regex-nfa-to-dot.test.ts
@@ -1,6 +1,18 @@
 import { expect, test } from "vitest";
 import { regexNFAToDot } from "./regex-nfa-to-dot.js";
 
+expect.addSnapshotSerializer({
+  test(val) {
+    return typeof val === "string" && val.startsWith("digraph");
+  },
+  serialize(val) {
+    // Return the DOT string as is so that I can copy
+    // the DOT lang output from the snapshot file and
+    // use it where I want without modifying it further.
+    return val;
+  },
+});
+
 test("can convert a simple regex to DOT lang", () => {
   expect(regexNFAToDot(`a`)).toMatchSnapshot();
 });
