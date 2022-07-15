4729eff1c68b525f08cb2ac96a587a5c5ea49195
Mubaraq Wahab <mub.wahab26@gmail.com>
2022-07-15 11:37:27 +0100

    complete regex pattern of 3.2

--- exercises/3_2/index.test.ts
+++ exercises/3_2/index.test.ts
@@ -3,19 +3,27 @@
 // and single pairs of < > and { } which may be nested
 // but not interleaved.
 
+import { rx } from "verbose-regexp";
 import { expect, test } from "vitest";
 
-// I don't understand the pattern
-const pattern = /^X*$/;
+const pattern = rx`
+  ^(
+    X
+    |\{(X|<X*>)*\} // Any number of Xs or < then Xs then > in {}
+    |<(X|\{X*\})*> // Any number of Xs or { then Xs then } in <>
+  )*$
+`;
 
-test.skip("matches the pattern", () => {
-  const strings = ["XXX<XX{X}XXX>X", "X{X}X<X>X{X}X<X>X"];
+/^(X*|\{X*<X*>|\{X*\})$/;
+
+test("matches the pattern", () => {
+  const strings = ["XXX<XX{X}XXX>X", "X{X}X<X>X{X}X<X>X", "XXX"];
   for (const s of strings) {
     expect(s).toMatch(pattern);
   }
 });
 
-test.skip("doesn't match the pattern", () => {
+test("doesn't match the pattern", () => {
   const strings = ["XXX<X<XX>>XX", "XX<XX{XX>XX}XX"];
   for (const s of strings) {
     expect(s).not.toMatch(pattern);
